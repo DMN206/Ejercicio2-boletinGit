0cd7e401b65b50cfecff86d2deadba56d520465c
Camilo <camilo.sh06@gmail.com>
2025-11-24 16:52:01 +0100

    Creada la función para calcular la media de nota de toda la clase en conjunto

--- src/main/java/boletinGit/Main.java
+++ src/main/java/boletinGit/Main.java
@@ -28,7 +28,7 @@ public class Main {
                     System.out.println("El area es: " +area);
                     break;
                 case 4:
-// insertar codigo aqui
+                    System.out.println(Notas.mediaClase(sc));
                     break;
                 case 0:
                     System.out.println("Saliendo del programa...");
